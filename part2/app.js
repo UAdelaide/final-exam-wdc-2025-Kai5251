3e660b5ef95dfbd41b9fc546a4a7c1063dc1416d
Kai5251 <a1937366@adelaide.edu.au>
2025-06-21 15:17:54 +0930

    Update app.js

--- part2/app.js
+++ part2/app.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const path = require('path');
+const session = require('express-session');
 require('dotenv').config();
 
 const app = express();
 
 // Middleware
 app.use(express.json());
+app.use(session({
+  secret: 'your-secret-key',
+  resave: false,
+  saveUninitialized: true
+}));
 app.use(express.static(path.join(__dirname, '/public')));
 
 // Routes
@@ -15,5 +21,4 @@ const userRoutes = require('./routes/userRoutes');
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
 
-// Export the app instead of listening here
 module.exports = app;
